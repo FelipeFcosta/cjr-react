a4e57206abbd3fff5a6c2c86793e6cfe905e8891
felipefcosta <felipecostasdc@gmail.com>
2021-10-07 02:06:15 -0300

    pagina principal esta em outro arquivo

--- src/App.js
+++ src/App.js
@@ -1,80 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react'    // um Hook
-import Todo from './components/Todo';
-import styled from "styled-components"
-
-const DivApp = styled.div`
-  text-align: center;
-  margin-inline: auto;
-  width: 500px;
-
-  > * {     // style all children
-    width: 100%;
-  }
-`
-
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  gap: 5px;
-`
-
-const Ul = styled.ul`
-  display: flex;
-  flex-direction: column;
-  padding-left: 0;
-`
+import Main from './pages/Main';
 
 
 function App() {
-  const [countTodo, setNumero] = useState(0)  // useState to re-render components
-  const [lista, setLista] = useState([])
-
-  const [title, setTitle] = useState("")
-  const [description, setDescription] = useState("")
-
-  function count(step) {
-    setNumero(prevNumero => prevNumero + step)
-    console.log("count")
-  }
-
-  function handleCreation(event) {
-    // todo synthetic event nos da um objeto event
-    event.preventDefault()  // prevent page reload, so we won't lose the states
-
-    if (title !== "") {
-      const nTodo = {title: title, description: description, checked: false, id: lista.length}
-      setLista(lista => [...lista, nTodo])
-
-      setTitle("")
-      setDescription("")
-      count(1);
-    }
-  }
-
-  // onSubmit, onChange sao synthetic events
-  // event.target.value (target - elemento do DOM que esta sendo afetado)
   return (
-    <DivApp>
-      <h1>{countTodo} coisas para fazer</h1>
-      <Form onSubmit={handleCreation} autoComplete="off">
-        <label>Title</label>
-        <input style={{padding: "0.5em"}} type="text" value={title} onChange={(event) => {setTitle(event.target.value)}}/>
-
-        <label>Description</label>
-        <textarea cols="15" rows="3" value={description} onChange={(event) => {setDescription(event.target.value)}}/>
-        <button>create</button>
-      </Form>
-
-      <Ul>
-        {lista.map( (listItem) =>   // pra cada item da lista, criar um li
-          <Todo key={listItem.id} listItem={listItem} leftTodo={count}/>
-        )}
-      </Ul>
-
-    </DivApp>
-  );
+    <Main></Main>
+  )
 }
 
 export default App;
